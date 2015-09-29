1bcad2b305074ffcb49bcfdb26371c4772eca422
jneG <jnje123ysdy>
2015-09-29 15:42:17 +0800

    add some code about ClassLoader.getResource()

--- src/main/java/testpackage/TestMain.java
+++ src/main/java/testpackage/TestMain.java
@@ -6,6 +6,24 @@ package testpackage;
  */
 public class TestMain {
     public static void main(String[] args) {
+//        testClass_getResource();
+        testClassLoader_getResource();
+    }
+
+    private static void testClassLoader_getResource() {
+        TestMain t = new TestMain();
+        System.out.println(t.getClass());
+        System.out.println(t.getClass().getClassLoader());
+        //此注释为下面这一行代码 TestMain.class.getResource("/") == t.getClass().getClassLoader().getResource("")
+        System.out.println(t.getClass().getClassLoader().getResource(""));
+        System.out.println(t.getClass().getClassLoader().getResource("/"));//null
+
+        System.out.println(t.getClass().getClassLoader().getResource("1.properties"));
+        System.out.println(t.getClass().getClassLoader().getResource("testpackage/2.properties"));
+        System.out.println(t.getClass().getClassLoader().getResource("testpackage/subpackage/3.properties"));
+    }
+
+    private static void testClass_getResource() {
         // 当前类(class)所在的包目录
         System.out.println(TestMain.class.getResource(""));
         // class path根目录
